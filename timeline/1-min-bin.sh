4e6ec40bae3a6e731f34cd5bbe43aed402af19d4
Emir Turkes <eturkes@bu.edu>
2018-01-04 14:44:47 +0900

    Add attribution

--- timeline/1-min-bin.sh
+++ timeline/1-min-bin.sh
@@ -26,6 +26,10 @@ i=1
 max=61
 while [ $i -lt $max ]
 do	
+	# Implementation derived from Ask Ubuntu, part of the Stack Exchange network
+	# https://askubuntu.com/questions/408775/add-seconds-to-a-given-date-in-bash
+	# Question asked by: Markus https://askubuntu.com/users/212156/markus
+	# Answer given by: steeldriver https://askubuntu.com/users/178692/steeldriver
 	start_secs=$(date +%s --date="${start_date}")
 	end_date="$(date '+%Y-%m-%d %H:%M:%S' --date="@$((start_secs + 60))")"
 
