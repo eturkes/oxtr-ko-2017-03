93b18e4dbf2c1c8bd604d8a739481672e951d742
Emir Turkes <eturkes@bu.edu>
2018-08-19 12:18:50 -0400

    Use latest pymice-notebook build

--- launch-docker.sh
+++ launch-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker run -it --rm -p 8888:8888 \
-	-v ${PWD}:/home/jovyan/work eturkes/pymice-notebook:py3.5.4v1 \
+	-v ${PWD}:/home/jovyan/work eturkes/pymice-notebook:py3.6.6v1 \
 	/bin/bash -c "source activate pymice && jupyter notebook"
